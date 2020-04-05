b6f492976e50458fd2856e7298dc1ad09b5cc850
Rakibul1294 <md.rakibul.1294@gmail.com>
2020-04-05 21:51:33 +0600

    hot-onion-restaurent node api

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: t
 const users = ["Asad", 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
 
 
-app.get('/foodItems', (req, res) => {
+app.get('/foodItem', (req, res) => {
     client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         const collection = client.db("onlineFoodStore").collection("foodType");
